d1232f15a500c6e06723d9ba5b63cd768262271b
ThiagoSuchi <thiagohenssuchi@gmail.com>
2026-06-02 16:06:55 -0400

    fix: Correção da baseURL para uar variaveis de ambiente

--- Frontend/src/api/config.ts
+++ Frontend/src/api/config.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'sfico-production.up.railway.app',
+    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3080',
     timeout: 10000,
     headers: {
         'Content-Type': 'application/json'
